4a374106e3db9bc1dc32a2ad10215f8994abe618
Joshua Nye <joshua.nye@gmail.com>
2020-08-12 15:08:53 -0400

    Improve test coverage

--- tests/Sequence/TerminatedTest.php
+++ tests/Sequence/TerminatedTest.php
@@ -25,9 +25,19 @@ class TerminatedTest extends TestCase
         self::assertEquals("anvil", $remaining);
     }
 
-    public function testTerminatedFailure()
+    public function testTerminatedFailureOnSecondParser()
     {
         $input = new Span("aircraftanvil");
+        $parser = Sequence::terminated(Bytes::tag("aircraft"), Bytes::tag(";"));
+        [$remaining, $output, $err] = $parser($input);
+        self::assertEquals(Error::ERR_TAG, $err);
+        self::assertNull($output);
+        self::assertNull($remaining);
+    }
+
+    public function testTerminatedFailureOnFirstParser()
+    {
+        $input = new Span("anvil;aardvark");
         $parser = Sequence::terminated(Bytes::tag("aircraft"), Bytes::tag(";"));
         [$remaining, $output, $err] = $parser($input);
         self::assertEquals(Error::ERR_TAG, $err);
